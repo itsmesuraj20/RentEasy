da8266a46613af606fec66d3ec567efb9edb7ae3
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-24 23:46:22 +0530

    New Deployment

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 className="text-4xl font-bold mb-6">Welcome to My Next.js App</h1>
+    </div>
+  );
+}
