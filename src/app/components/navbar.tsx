41ec71c36e49de4b3c3e71b453c5d63f9e00bdab
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-05-29 01:42:47 +0530

    NavBar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,11 +4,16 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-lg font-bold">
+        <Link href="https://google.com" className="text-white text-lg font-bold">
           RentEasy
         </Link>
-        <div className="space-x-4">
-          {/* Other navigation links/buttons go here */}
+        <div className="space-x-6">
+          <Link href="/list-property" className="text-white hover:underline">
+            List Your Property
+          </Link>
+          <Link href="/contact" className="text-white hover:underline">
+            Contact Us
+          </Link>
         </div>
       </div>
     </nav>
